d7f4a78bcc996d712b971e3f772b9d848aa21788
Plsr <christian@gigs.com>
2024-03-24 11:29:25 +0100

    Smol updates

--- app/page.tsx
+++ app/page.tsx
@@ -30,10 +30,7 @@ export default function Home() {
   );
 
   return (
-    <div className="mx-auto max-w-xl py-8">
-      <h1 className="mb-8 text-center text-2xl font-black">
-        Next.js + Contentlayer Example
-      </h1>
+    <div className="max-w-xl py-8">
       {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
       ))}
